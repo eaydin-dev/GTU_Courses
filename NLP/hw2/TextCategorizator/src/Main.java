eef920e15f5f3fad7e72b262b592df071f6e17ec
EA <eyypaydin@gmail.com>
2017-05-13 22:27:36 +0300

    Training code completed and classifying with knn.

--- NLP/hw2/TextCategorizator/src/Main.java
+++ NLP/hw2/TextCategorizator/src/Main.java
@@ -1,7 +1,10 @@
+import javafx.util.Pair;
 import utils.ReadFile;
 import utils.Serialization;
 
 import java.io.File;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.*;
 
 public class Main {
@@ -9,10 +12,38 @@ public class Main {
     private static final String DATA_PATH = "C:\\Users\\EA\\Desktop\\raw_texts\\";
     private static final String MODEL_PATH = "tc.model";
     private static final String TEST_PATH = "tc.test";
-    private static Map<File, String> trainingFiles = new HashMap<>();
-    private static Map<File, String> testFiles = new HashMap<>();
 
     public static void main(String[] args) {
+
+        TextCategorizator model;
+        List<Map.Entry<File, String>> tests;
+        if (!(new File(MODEL_PATH).exists())) {
+            System.out.println("> Model file couldn't found. Training again.");
+            Pair<TextCategorizator, List<Map.Entry<File, String>>> pair = trainModel();
+            model = pair.getKey();
+            tests = pair.getValue();
+        }
+        else {
+            System.out.println("> Loading model file.");
+            model = (TextCategorizator) Serialization.readObject(MODEL_PATH);
+            Map<File, String> testMap = (Map<File, String>) Serialization.readObject(TEST_PATH);
+            tests = new ArrayList<> (testMap.entrySet());
+            System.out.println("> Model reading done.");
+        }
+
+
+        System.out.println("Actual: " + tests.get(0).getValue());
+        System.out.println("Result: " + model.classifyKnn(tests.get(0).getKey(), 3));
+
+
+    }
+
+    private static Pair<TextCategorizator, List<Map.Entry<File, String>>> trainModel() {
+        long start = System.currentTimeMillis();
+
+        Map<File, String> trainingFiles = new HashMap<>();
+        Map<File, String> testFiles = new HashMap<>();
+
         Map<File, String> files = new HashMap<>();
         getAllPaths(new File(DATA_PATH), files);
         List<Map.Entry<File, String>> data = new ArrayList<>(files.entrySet());
@@ -30,9 +61,15 @@ public class Main {
         }
 
         TextCategorizator tc = new TextCategorizator();
-        tc.trainModel(trainingFiles);
+        tc.trainModel(trainingFiles, MODEL_PATH);
 
-        System.out.println(tc);
+        Serialization.writeObject(testFiles, TEST_PATH);
+
+        long end = System.currentTimeMillis();
+        NumberFormat formatter = new DecimalFormat("#0.00000");
+        System.out.println("Training time is " + formatter.format((end - start) / 1000d) + " seconds.");
+
+        return new Pair<>(tc, new ArrayList<>(testFiles.entrySet()));
     }
 
     private static void getAllPaths(File root, Map<File, String> fileCategoryMap) {
--- NLP/hw2/TextCategorizator/src/TextCategorizator.java
+++ NLP/hw2/TextCategorizator/src/TextCategorizator.java
@@ -1,23 +1,132 @@
 import utils.ReadFile;
+import utils.Serialization;
 
 import java.io.File;
+import java.io.Serializable;
 import java.util.*;
+import java.util.stream.Collectors;
 
-public class TextCategorizator {
 
-    private List<String> words = null;
+/**
+ * TODO: for idf, read each file while keeping the histogram of words for each file.
+ * TODO: this way, for each term, the overhead of opening and reading all files will be eliminated
+ */
+
+public class TextCategorizator implements Serializable{
+
+    private transient List<String> terms = null;
     private Map<File, String> trainingFiles;
-    private Map<String, Double> idfs;
+    private transient Map<String, Double> idfs;
+    private Map<File, double[]> tfidfMap;
+    private Set<String> classes;
 
-    public void trainModel(Map<File, String> trainingFiles) {
+    public void trainModel(Map<File, String> trainingFiles, String outputModel) {
+
         this.trainingFiles = trainingFiles;
+        System.out.println("> Getting words.");
         getWords();
+        System.out.println("> Calculating idfs.");
         calculateIdfs();
+        tfidfMap = new HashMap<>();
+        classes = new HashSet<>();
+
+        int c = 1, size = trainingFiles.size();
+        for (Map.Entry<File, String> entry : trainingFiles.entrySet()) {
+            System.out.println(String.format("%3d/%d -> %s", (c++), size, entry.getKey().getName()));
+            tfidfMap.put(entry.getKey(), getTfidfVector(entry.getKey()));
+            classes.add(entry.getValue());
+        }
+
+        if (outputModel != null)
+            Serialization.writeObject(this, outputModel);
+    }
+
+    public String classifyKnn(File file, int k) {
+        double[] givenVec = getTfidfVector(file);
+        Map<File, Double> distances = new HashMap<>();
+        for (Map.Entry<File, double[]> entry : tfidfMap.entrySet()) {
+            distances.put(entry.getKey(), cosSimilarity(givenVec, entry.getValue()));
+        }
+
+        distances = sortByValue(distances, true);
+
+        List<File> sortedFiles = new ArrayList<>(distances.keySet());
+        sortedFiles = sortedFiles.subList(0, k);
+        Map<String, Integer> counts = new HashMap<>();
+        for (String category : classes) {
+            counts.put(category, countClass(sortedFiles, category));
+        }
+
+        return (new ArrayList<>(sortByValue(counts, false).keySet())).get(0);
+    }
+
+    /**
+     * Taken from: http://stackoverflow.com/questions/109383/sort-a-mapkey-value-by-values-java
+     * @param map
+     * @param <K>
+     * @param <V>
+     * @return
+     */
+    private <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map, boolean reverse) {
+
+        return map.entrySet()
+                .stream()
+                .sorted((reverse ? Map.Entry.comparingByValue(Collections.reverseOrder()) : Map.Entry.comparingByValue()))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (e1, e2) -> e1,
+                        LinkedHashMap::new
+                ));
+    }
+
+    private int countClass(List<File> files, String c) {
+        int counter = 0;
+        for (File file : files) {
+            if (trainingFiles.get(file).equals(c))
+                ++counter;
+        }
+
+        return counter;
+    }
+
+    public Map<File, double[]> getTfidfMap() {
+        return tfidfMap;
+    }
+
+    private double cosSimilarity(double[] vec1, double[] vec2) {
+        return dotProduct(vec1, vec2) / (Math.sqrt(norm(vec1)) * Math.sqrt(norm(vec2)));
+    }
+
+    private double norm(double[] vec) {
+        double norm = 0.0;
+        for (double aVec : vec) norm += Math.pow(aVec, 2);
+        return norm;
+    }
+
+    private double dotProduct(double[] vec1, double[] vec2) {
+        double dotProduct = 0.0;
+        for (int i = 0; i < vec1.length; i++)
+            dotProduct += vec1[i] * vec2[i];
+        return dotProduct;
+    }
+
+    /**
+     * Get tfidf vector of the given file.
+     * @param file
+     * @return
+     */
+    private double[] getTfidfVector(File file) {
+        double[] tfidfs = new double[terms.size()];
+        int i = 0;
+        for (String term : terms)
+            tfidfs[i++] = tfidf(term, file);
+
+        return tfidfs;
     }
 
     /**
      * Gets the unique words of all training documents.
-     * @param trainingFiles
      */
     private void getWords() {
         Set<String> set = new HashSet<>();
@@ -34,7 +143,7 @@ public class TextCategorizator {
             set.addAll(Arrays.asList(tokens));
         }
 
-        words = new ArrayList<>(set);
+        terms = new ArrayList<>(set);
     }
 
     /**
@@ -105,14 +214,14 @@ public class TextCategorizator {
      */
     private void calculateIdfs() {
         idfs = new HashMap<>();
-        for (String term : words) {
+        for (String term : terms) {
             idfs.put(term, idf(term));
         }
     }
 
     @Override
     public String toString() {
-        String s = "Total words: " + (words == null ? 0 : words.size());
+        String s = "Total terms: " + (terms == null ? 0 : terms.size());
 
         return s;
     }
